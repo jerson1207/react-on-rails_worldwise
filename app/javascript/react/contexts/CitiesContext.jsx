dcf60016a54ea704568f6602c8b6990ac0dadd4a
jersonjabat <jersonjabatstudent@gmail.com>
2024-11-11 11:27:46 +0800

    replace useState with useReducer

--- app/javascript/react/contexts/CitiesContext.jsx
+++ app/javascript/react/contexts/CitiesContext.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
-import { createContext, useState, useEffect, useContext } from "react";
+import React, {
+  createContext,
+  useEffect,
+  useContext,
+  useReducer,
+} from "react";
 
 const CitiesContext = createContext();
+
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: "",
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'loading': 
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case 'cities/loaded':
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+    case 'city/loaded':
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload
+      };
+    case 'city/created':
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: {},
+      }
+    case 'city/deleted':
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+      }
+    case "rejected":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+    default:
+      throw new Error("Unknown error type");
+  }
+}
 
 const getCsrfToken = () => {
   const meta = document.querySelector('meta[name="csrf-token"]');
@@ -9,42 +64,48 @@ const getCsrfToken = () => {
 };
 
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({})
+  const [{cities, isLoading, currentCity, error}, dispatch] = useReducer(reducer, initialState)
+  // const [cities, setCities] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [currentCity, setCurrentCity] = useState({})
 
   useEffect( function () {
     async function fetchCities() {
+      // setIsLoading(true)
+      dispatch({ type: "loading" })
       try{
-        setIsLoading(true)
         const res = await fetch(`/api/v1/cities`)
         const data = await res.json();
-        setCities(data.cities)
+        // setCities(data.cities)
+        dispatch({type: 'cities/loaded', payload: data.cities})
       } catch {
-        alert('there was an error loading the data')
-      } finally {
-        setIsLoading(false)
+        // alert('there was an error loading the data')
+        dispatch({type: 'rejected', payload: "there was an error loading the cities"})
       }
     }
     fetchCities();
   }, []);
 
   async function getCity(id) {
+    if (Number(id) === currentCity.id) return;
+
+    // setIsLoading(true)
+    dispatch({ type: "loading" })
     try{
-      setIsLoading(true)
       const res = await fetch(`/api/v1/cities/${id}`)
       const data = await res.json();
-      setCurrentCity(data)
+      // setCurrentCity(data)
+      dispatch({type: 'city/loaded', payload: data})
     } catch {
-      alert('there was an error loading the data')
-    } finally {
-      setIsLoading(false)
+      // alert('there was an error loading the data')
+      dispatch({type: 'rejected', payload: "there was an error loading the city"})
     }
   }
 
   async function createCity(newCity) {
+    // setIsLoading(true)
+    dispatch({ type: "loading" })
     try {
-      setIsLoading(true)
       const response = await fetch('/api/v1/cities', {
         method: 'POST',
         headers: {
@@ -54,38 +115,47 @@ function CitiesProvider({ children }) {
         body: JSON.stringify(newCity)
       });
 
-      if (response.ok) { 
-        const addedCity = await response.json(); 
-        setCities(prevCities => [...prevCities, addedCity]);
-      }
-    } catch (error) {
-      console.error('Error creating post:', error);
-    } finally {
-      setIsLoading(false)
+      const data = await response.json(); 
+      dispatch({type: 'city/created', payload: data })
+      // if (response.ok) { 
+      //   setCities(prevCities => [...prevCities, addedCity]);
+      // }
+    } catch {
+      // console.error('Error creating post:', error);
+      dispatch({type: 'rejected', payload: "there was an error loading the city"})
     }
   }
 
   async function deleteCity(id) {
+    // setIsLoading(true)
+    dispatch({ type: "loading" })
     try {
-      setIsLoading(true)
       await fetch(`/api/v1/cities/${id}`, {
         method: 'DELETE',
         headers: {
           'X-CSRF-Token': getCsrfToken()
         }
       });
-      setCities((cities) => cities.filter((city) => city.id !== id));
-    } catch (error) {
-      console.error('Error creating post:', error);
-    } finally {
-      setIsLoading(false)
+      // setCities((cities) => cities.filter((city) => city.id !== id));
+      dispatch({type: 'city/deleted', payload: id })
+    } catch{
+      dispatch({type: 'rejected', payload: "there was an error loading the city"})
     }
   }
 
   return (
     <CitiesContext.Provider 
-      value={ { cities, isLoading, currentCity, getCity, createCity, deleteCity } }>
-    {children}
+      value={{ 
+        cities, 
+        isLoading, 
+        currentCity, 
+        error, 
+        getCity, 
+        createCity, 
+        deleteCity 
+      }}
+    >
+      {children}
     </CitiesContext.Provider>
   );
 }
